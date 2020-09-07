b07a193e549848066cc351a88c4025ae15c53c05
愿山聖 <asjstksmr@gmail.com>
2020-09-07 23:50:46 +0900

    lesson-2

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-alert('Hello World');
+// alert('Hello World');
+
+
+// var hello = "Hello World";
+// alert(hello);
+
+
+// var str1 = 'Hello';
+// var str2 = 'World!!';
+// alert(str1 + str2);
+
+
+// var orange = 100;
+// var apple = 120;
+
+// if(orange < apple){
+// 	alert('みかんの値段がりんごより安い');
+// }
+// else if(orange == apple){
+// 	alert('みかんとりんごが同じ値段')
+// }
+// else{
+// 	alert('みかんの値段がりんごより高い')
+// }
+
+
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+// 	num = num * 2;
+// 	count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'をこえるのに必要だった回数は' + count + '回です。');
+
+
+
+var i;
+var num = 0;
+
+for(i = 1; i < 11; i++){
+	num = num + i;
+}
+
+alert('1から10まで足算した結果は' + num + 'です');
