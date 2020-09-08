e2164cee188c31bcd8d3e4b4da50e940a7356cd8
愿山聖 <asjstksmr@gmail.com>
2020-09-08 11:27:15 +0900

    lesson4

--- app.js
+++ app.js
@@ -63,54 +63,60 @@
 // alert(promptStr)
 
 
-var user_hand = prompt('ジャンケンの手をグー、チョキ、パーから選んでください。');
-var js_hand = getJShand();
-var judge = winLose(user_hand, js_hand)
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' +　js_hand + 'です。\n結果は'　+ judge + 'です。');
+// var user_hand = prompt('ジャンケンの手をグー、チョキ、パーから選んでください。');
+// var js_hand = getJShand();
+// var judge = winLose(user_hand, js_hand)
+// alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' +　js_hand + 'です。\n結果は'　+ judge + 'です。');
 
 
-function getJShand(){
-	var js_hand_num = Math.floor( Math.random() * 3 );
-	// Math.floorが小数点を切り捨て、Math.randomが0以上1未満の値をランダムで返す
+// function getJShand(){
+// 	var js_hand_num = Math.floor( Math.random() * 3 );
+// 	// Math.floorが小数点を切り捨て、Math.randomが0以上1未満の値をランダムで返す
 
-	if(js_hand_num == 0){
-		js_hand = "グー";
-	}else if(js_hand_num == 1){
-		js_hand = "チョキ";
-	}else if(js_hand_num == 2){
-		js_hand = "パー";
-	}
+// 	if(js_hand_num == 0){
+// 		js_hand = "グー";
+// 	}else if(js_hand_num == 1){
+// 		js_hand = "チョキ";
+// 	}else if(js_hand_num == 2){
+// 		js_hand = "パー";
+// 	}
 
-	return js_hand;
-}
+// 	return js_hand;
+// }
 
 
-function winLose(user, js){
-	var winLoseStr;
+// function winLose(user, js){
+// 	var winLoseStr;
 
-	if(user == "グー"){
-		if(js == "グー"){
-			winLoseStr = "あいこ";
-		}else if(js == "チョキ"){
-			winLoseStr = "勝ち";
-		}else if(js == "パー"){
-			winLoseStr = "負け";
-		}
-	}else if(user == "チョキ"){
-		if(js == "グー"){
-			winLoseStr = "負け";
-		}else if(js == "チョキ"){
-			winLoseStr = "あいこ";
-		}else if(js_hand == "パー");
-		winLoseStr = "勝ち";
-	}else if(user == "パー"){
-		if(js == "グー"){
-			winLoseStr = "勝ち";
-		}else if(js_hand == "チョキ"){
-			winLoseStr = "負け";
-		}else if(js_hand == "パー"){
-			winLoseStr = "あいこ";
-		}
-	}
-	return winLoseStr;
-}
+// 	if(user == "グー"){
+// 		if(js == "グー"){
+// 			winLoseStr = "あいこ";
+// 		}else if(js == "チョキ"){
+// 			winLoseStr = "勝ち";
+// 		}else if(js == "パー"){
+// 			winLoseStr = "負け";
+// 		}
+// 	}else if(user == "チョキ"){
+// 		if(js == "グー"){
+// 			winLoseStr = "負け";
+// 		}else if(js == "チョキ"){
+// 			winLoseStr = "あいこ";
+// 		}else if(js_hand == "パー");
+// 		winLoseStr = "勝ち";
+// 	}else if(user == "パー"){
+// 		if(js == "グー"){
+// 			winLoseStr = "勝ち";
+// 		}else if(js_hand == "チョキ"){
+// 			winLoseStr = "負け";
+// 		}else if(js_hand == "パー"){
+// 			winLoseStr = "あいこ";
+// 		}
+// 	}
+// 	return winLoseStr;
+// }
+
+
+
+$(document).ready(function () {
+	$('body').html('<h1>Hello jQuery!!</h1>');
+});
