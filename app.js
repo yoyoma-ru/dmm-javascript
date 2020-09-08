a67ba5979dbb10c1f21aadf72fc713d5ea0cc904
愿山聖 <asjstksmr@gmail.com>
2020-09-08 11:42:47 +0900

    lesson-5

--- app.js
+++ app.js
@@ -117,6 +117,32 @@
 
 
 
-$(document).ready(function () {
-	$('body').html('<h1>Hello jQuery!!</h1>');
+// $(document).ready(function () {
+// 	$('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+
+
+// $(function(){
+// 	$('.box1').css({
+// 		'background-color': '#0000FF',
+// 		'height': '100px'
+// 	});
+// });
+
+// $(function(){
+// 	$('.box1').slideDown();
+// });
+
+// $(function(){
+// 	$('.box1').slideUp();
+// });
+
+// $(function(){
+// 	$('.box1').show();
+// 	$('.box1').css({'background-color': '#0000FF'});
+// });
+
+$(function(){
+	$('.box1').hide();
 });
