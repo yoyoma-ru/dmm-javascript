39ffef597886d5882e545590ad77c3ad522e14d9
愿山聖 <asjstksmr@gmail.com>
2020-09-08 12:12:19 +0900

    lesson7

--- app.js
+++ app.js
@@ -171,11 +171,44 @@
 // });
 
 
+// $(function(){
+// 	$('.box1').on('click', function(){
+// 		$('.box1').addClass('box1-ext');
+// 	});
+// 	$('.box1').mouseover(function(){
+// 		$('.box1').removeClass('box1-ext');
+// 	});
+// });
+
+
+
+// lesson7
+
+// $(function(){
+// 	$('.bg1').on('click', function(){
+// 		$('.bg1').slideUp();
+// 	});
+// 	$('.bg2').on('click', function(){
+// 		$('.bg2').slideUp();
+// 	});
+// 	$('.bg3').on('click', function(){
+// 		$('.bg3').slideUp();
+// 	});
+// 	$('.bg4').on('click', function(){
+// 		$('.bg4').slideUp();
+// 	});
+// });
+
+
+// $(function(){
+// 	$('.box1').on('click', function(){
+// 		$(this).slideUp();
+// 	});
+// })
+
+
 $(function(){
-	$('.box1').on('click', function(){
-		$('.box1').addClass('box1-ext');
+	$('button').on('click', function(){
+		$('ul').children().css('color','red');
 	});
-	$('.box1').mouseover(function(){
-		$('.box1').removeClass('box1-ext');
-	});
-});
+})
