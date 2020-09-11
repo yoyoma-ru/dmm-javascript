0e6cb18953f9dab583fe446832b34831be12dec2
愿山聖 <asjstksmr@gmail.com>
2020-09-11 23:52:59 +0900

    [aad]補足

--- SlideShow-Lesson/js/script.js
+++ SlideShow-Lesson/js/script.js
@@ -2,11 +2,14 @@ $(".theTarget").skippr({
 	transition : 'slide',
 	speed : 1000,
 	easing : 'easeOutQuart',
+	// 初速が速い
 	navType : 'block',
+	// 下に表示される枚数と場所を表す表示
 	childrenElemntType : 'div',
 	arrows : true,
 	autoPlay : true,
 	autoPlayDuration : 3000,
 	keyboardOnAlways : true,
+	// キーボードの矢印キーで操作可能
 	hedePrevious : true,
 });
