7e9cfb60e7a9e32f76a41e8da77a706d9a85ba1e
愿山聖 <asjstksmr@gmail.com>
2020-11-18 11:01:45 +0900

    chapter5

--- app3.js
+++ app3.js
@@ -1,7 +1,40 @@
-$(document).ready(function(){
-	$("body").html("<h1>Hello jQuery!!<h1>");
-});
+// $(document).ready(function(){
+// 	$("body").html("<h1>Hello jQuery!!<h1>");
+// });
+
+// $(function(){
+// 	$("body").html("<h2>がんばろう<h2>");
+// });
+
+
+// CSSの変更
+// $(function(){
+// 	$(".box1").css({
+// 		'background-color': '#0000FF',
+// 		'height': '100px',
+// 	});
+// });
+
+
+// スライドダウン
+// $(function(){
+// 	$(".box1").slideDown();
+// });
+
+
+// スライドアップ
+// $(function(){
+// 	$(".box1").slideUp();
+// });
+
+
+// 要素の表示
+// $(function(){
+// 	$(".box1").show();
+// 	$(".box1").css('background-color', '#0000FF');
+// });
+
 
 $(function(){
-	$("body").html("<h2>がんばろう<h2>");
+	$(".box1").hide();
 });
