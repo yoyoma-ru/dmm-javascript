e3cb694882dc185c1f2423acbebdf50abd9d0578
愿山聖 <asjstksmr@gmail.com>
2020-11-18 09:36:10 +0900

    chapter2

---
+++ app2.js
@@ -0,0 +1,47 @@
+// alert("hello world");
+
+// var hello = "Hello World";
+// alert(hello);
+
+
+// alert("Hello"+" World");
+// var str1 = 'Hello';
+// var str2 = "World!";
+// alert(str1 + str2);
+
+
+// var orange = 100;
+// var apple = 120;
+// if(orange < apple){
+// 	alert("みかんの値段がりんごより安い");
+// }
+// else if(orange == apple){
+// 	alert("みかんとリンゴが同じ値段");
+// }
+// else{
+// 	alert("みかんの値段がりんごより高い");
+// }
+
+
+
+// var max = 100;
+// var num = 1;
+// var count = 0;
+// while(num < max){
+// 	num = num * 2;
+// 	count = count + 1;
+// 	alert(num);
+// }
+// alert("2をかけ続けて"
+// 	+ max
+// 	+ "を超えるのに必要だった回数は"
+// 	+ count
+// 	+ "回です");
+
+
+var i = 1;
+var num = 0;
+for(i; i < 11; i++){
+	num = num + i;
+}
+alert("1から10まで足し算した結果は" + num + "です。");
